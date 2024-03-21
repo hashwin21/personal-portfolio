cb7608c017b08bde168f3427e9d5b980bc571f64
hashwin21 <161727492+hashwin21@users.noreply.github.com>
2024-03-21 21:42:39 +0530

    Add files via upload

---
+++ HTML/portfolio.js
@@ -0,0 +1,20 @@
+function openInsta() {
+    window.open("https://www.instagram.com", "_blank");
+}
+
+function openFacebook() {
+    window.open("https://www.facebook.com", "_blank");
+}
+
+function openLinkedIn() {
+    window.open("https://www.linkedin.com", "_blank");
+}
+
+function openGitHub() {
+    window.open("https://www.github.com", "_blank");
+}
+
+function openVideo() {
+    var videoUrl = 'video.mp4.mp4';
+    window.open(videoUrl, "_blank");
+}
